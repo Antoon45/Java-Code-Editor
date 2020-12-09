f50dab0d9d5755af28b2e0cdfab6139f260ee8b3
Antoon45 <anton.warnstrom@gmail.com>
2020-12-09 13:36:06 +0200

    First version of layout, badabim badabum

---
+++ javafx_starter_fxml/src/main/java/ax/ha/it/starter/AppController.java
@@ -0,0 +1,11 @@
+package ax.ha.it.starter;
+
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class AppController {
+
+    public VBox root;
+    public ScrollPane directory;
+    public ScrollPane codeArea;
+}
