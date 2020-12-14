8e879046c7550657e25453dcd923f365c9dfcf06
Antoon45 <anton.warnstrom@gmail.com>
2020-12-14 22:17:17 +0200

    Layout done

--- javafx_starter_fxml/src/main/java/ax/ha/it/starter/AppController.java
+++ javafx_starter_fxml/src/main/java/ax/ha/it/starter/AppController.java
@@ -1,6 +1,8 @@
 package ax.ha.it.starter;
 
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.VBox;
 
 public class AppController {
@@ -8,4 +10,5 @@ public class AppController {
     public VBox root;
     public ScrollPane directory;
     public ScrollPane codeArea;
+    public TabPane tabs;
 }
