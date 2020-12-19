e81c7819964d083cef40a9f542468d789dc4c913
AndreasTallberg <andreas.tallberg@ha.ax>
2020-12-19 20:45:42 +0200

    added add tab button

--- javafx_starter_fxml/src/main/java/ax/ha/it/starter/AppController.java
+++ javafx_starter_fxml/src/main/java/ax/ha/it/starter/AppController.java
@@ -6,7 +6,10 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class AppController {
@@ -15,4 +18,21 @@ public class AppController {
     public ScrollPane directory;
     public ScrollPane codeArea;
     public TabPane tabs;
+    int tabNum = 2;
+
+    @FXML TabPane tabby;
+
+    @FXML
+    private void closeButton(){
+        System.exit(0);
+    }
+
+    @FXML
+    private void addNewTab() {
+        final Tab tab = new Tab("Untitled Tab" + tabNum);
+        tabby.getTabs().add(tab);
+        tabby.getSelectionModel().select(tab);
+        tabNum++;
+    }
 }
+
--- javafx_starter_fxml/src/main/java/ax/ha/it/starter/StarterApp.java
+++ javafx_starter_fxml/src/main/java/ax/ha/it/starter/StarterApp.java
@@ -1,10 +1,12 @@
 package ax.ha.it.starter;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
@@ -21,7 +23,6 @@ public class StarterApp extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(StarterApp.class.getResource("layout.fxml"));
-
 
         // Note: Exclude width/height params to only use as much space as needed
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
